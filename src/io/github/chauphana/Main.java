a283e16fdea7c0da896ce3cf99f02210ba21b07e
Anderson Chauphan <chauphana@outlook.com>
2019-04-08 20:16:44 -0500

    Initial commit - doesn't even work yet

---
+++ src/io/github/chauphana/Main.java
@@ -0,0 +1,62 @@
+package io.github.chauphana;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.ByteArrayInputStream;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FTPClient client = new FTPClient();
+        //https://stackoverflow.com/questions/11202215/how-to-copy-a-file-on-the-ftp-server-to-a-directory-on-the-same-server-in-java
+        try {
+            client. connect("na47.pebblehost.com");
+            client.login("chauphana@outlook.com.75155", "209407880ac");
+            System.out.println(client.getStatus());
+            if (client.isConnected()) {
+                System.out.println("is connected: " + client.isConnected());
+                System.out.println(client.getStatus());
+
+                System.out.println(client.getStatus("/plugins"));
+
+                client.enterLocalPassiveMode();
+                client.setFileType(FTP.BINARY_FILE_TYPE);
+                String fileLocation = "eula.txt";
+                File file = new File(fileLocation);
+                //ByteArrayInputStream(FileUtils.readFileToByteArray(fileLocation));
+                FileInputStream inputStream = new FileInputStream(new File(fileLocation));
+                boolean result = client.storeFile("/backups/eula.txt", inputStream);
+                inputStream.close();
+                if (result) {
+                    System.out.println("upload done");
+                }
+
+            }
+            client.logout();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                client.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+
+
+}
